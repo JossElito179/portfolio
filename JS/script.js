4c1902b1741700a686781fe70a0bf9b1294c691b
JossElito179 <118587500+JossElito179@users.noreply.github.com>
2023-12-19 21:35:44 +0300

    spinner added

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,11 @@ const navbarLinks=document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 })
+
+document.onreadystatechange = function () {
+    var spinnerOverlay = document.querySelector('.page_loader');
+
+    if (document.readyState === 'complete') {
+        spinnerOverlay.style.display = 'none';
+    }
+};
